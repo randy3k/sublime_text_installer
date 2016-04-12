eedb96c07c8f11d0da24706383785d4c522625ae
Randy Lai <randy.cs.lai@gmail.com>
2016-04-12 16:20:11 -0400

    use file to flag bootstrap status

--- helper.py
+++ helper.py
@@ -11,58 +11,63 @@ def plugin_loaded():
     def kill_subl(restart=False):
         if sublime.platform() == "osx":
             if sublime.version() > "3000":
-                cmd = "sleep 1; killall 'Sublime Text'; sleep 3; "
+                cmd = "sleep 1; killall 'Sublime Text'; sleep 1; "
                 if restart:
                     cmd = cmd + "osascript -e 'tell application \"Sublime Text\" to activate'"
             else:
-                cmd = "sleep 1; killall 'Sublime Text 2'; sleep 3; "
+                cmd = "sleep 1; killall 'Sublime Text 2'; sleep 1; "
                 if restart:
                     cmd = cmd + "osascript -e 'tell application \"Sublime Text 2\" to activate'"
         elif sublime.platform() == "linux":
-            cmd = "sleep 1; killall 'sublime_text'; sleep 3; "
+            cmd = "sleep 1; killall 'sublime_text'; sleep 1; "
             if restart:
                 cmd = cmd + "subl"
         elif sublime.platform() == "windows":
-            cmd = "sleep 1 & taskkill /F /im sublime_text.exe & sleep 3 "
+            cmd = "sleep 1 & taskkill /F /im sublime_text.exe & sleep 1 "
             if restart:
                 cmd = cmd + "& \"C:\\st\\sublime_text.exe\""
 
         subprocess.Popen(cmd, shell=True)
 
+    def touch(file_name):
+        f = os.path.join(
+            sublime.packages_path(),
+            "0_install_package_control_helper",
+            file_name)
+        open(f, 'a').close()
+
     def check_bootstrap():
         if pc_settings.get("bootstrapped", False):
+            touch("bootstrapped")
             kill_subl(True)
         else:
             sublime.set_timeout(check_bootstrap, 20)
 
     def check_dependencies():
         if sublime.version() > "3000" and 'Package Control' in sys.modules:
-            manager = sys.modules[
-                        'Package Control'].package_control.package_manager.PackageManager()
+            package_control = sys.modules['Package Control'].package_control
         elif sublime.version() < "3000" and 'package_control' in sys.modules:
-            manager = sys.modules['package_control'].package_manager.PackageManager()
+            package_control = sys.modules['package_control']
         else:
             sublime.set_timeout(check_dependencies, 20)
             return
 
+        manager = package_control.package_manager.PackageManager()
         required_dependencies = set(manager.find_required_dependencies())
 
         def _check_dependencies():
             installed_dependencies = set(manager.list_dependencies())
             missing_dependencies = required_dependencies - installed_dependencies
             if len(missing_dependencies) == 0:
-                success = os.path.join(
-                    sublime.packages_path(),
-                    "0_install_package_control_helper",
-                    "success")
-                open(success, 'a').close()
+                touch("success")
                 kill_subl()
             else:
                 sublime.set_timeout(_check_dependencies, 20)
 
         _check_dependencies()
 
-    if not pc_settings.get("bootstrapped", False):
+    if not os.path.exists(os.path.join(sublime.packages_path(), 
+        "0_install_package_control_helper", "bootstrapped")):
         check_bootstrap()
     else:
         check_dependencies()
